d6a398868eefff8358b8c3389492e34612ac1dad
Julian Eichen <juleichen@aol.com>
2022-10-03 14:12:25 +0200

    conditional output to matchdaytable

--- src/main/java/com/myprojects/SBleague/SpringbootLeagueApplication.java
+++ src/main/java/com/myprojects/SBleague/SpringbootLeagueApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootLeagueApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootLeagueApplication.class, args);
 	}
