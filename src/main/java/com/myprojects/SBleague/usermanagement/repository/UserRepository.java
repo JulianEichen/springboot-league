c035b9a9220a0954a0b196b18a351cff0d0155d2
Julian Eichen <juleichen@aol.com>
2022-10-06 13:28:40 +0200

    backend for user management added

---
+++ src/main/java/com/myprojects/SBleague/usermanagement/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.myprojects.SBleague.usermanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.myprojects.SBleague.usermanagement.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+}
---
+++ src/main/java/com/myprojects/SBleague/usermanagement/web/AuthController.java
@@ -0,0 +1,9 @@
+package com.myprojects.SBleague.usermanagement.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthController {
+	
+
+}
